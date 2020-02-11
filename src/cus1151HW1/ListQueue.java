6c616c6094a8c2ada08eb80e87bbfa989ad1a9bb
ledong1 <ledong.shi17@stjohns.edu>
2020-02-11 09:13:53 -0500

    create folder

---
+++ src/cus1151HW1/ListQueue.java
@@ -0,0 +1,31 @@
+package cus1151HW1;
+
+public class ListQueue {
+	private class Node{
+		//TODO Implement Linked List Node
+		private data;
+		private 
+	}
+	
+	//Class variables here, if necessary
+	
+	
+	public ListQueue(){
+		//TODO Implement constructor
+	}
+	
+	public void enqueue(String toInput) {
+		// TODO Implement enqueue
+	}
+	
+	public String dequeue(){
+		// TODO Implement dequeue
+		return null;
+	}
+	
+	public String front(){
+		// TODO Implement front
+		return null;
+	}
+
+}
